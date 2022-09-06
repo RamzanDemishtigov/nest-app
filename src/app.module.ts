1614f0824a05a56742fa63360708d6aa5152dbd6
Magomed <magomed.demishtigov@yandex.ru>
2022-09-06 19:08:46 +0300

    Final version

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,18 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './core/database/database.module';
 import { UsersService } from './modules/users/users.service';
 import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { PostsModule } from './modules/posts/posts.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     UsersModule,
+    AuthModule,
+    PostsModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController,],
   providers: [AppService,],
 })
 export class AppModule {}
